400bfd6e17d30ad9401568a49bb61483e334feb9
JC CorMan <cordovez@el-cordovez.com>
2023-09-01 15:11:22 +0200

    delete some unnecessary files

--- flashcards/models/flash_card_content.py
+++
@@ -1,8 +0,0 @@
-
-from abc import ABC 
-from typing import Optional
-
-class Content(ABC):
-    """ Basic representation of a single flashcard """
-    front: str
-    back: Optional[tuple] = None
--- flashcards/models/inversions.py
+++
@@ -1,7 +0,0 @@
-
-from enum import Enum
-
-class Inversion(Enum):
-    ROOT = ("R", "3", "5")
-    FIRST = ( "3", "5", "R")
-    SECOND = ("5", "R", 3)
